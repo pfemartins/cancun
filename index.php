3c47cebbcd1a2f2989972db83cde349dfad07f5a
Stephanie Jelalian <stephaniejelalian@Stephanies-MacBook-Pro.local>
2017-07-31 11:22:54 -0600

    added twig

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
-<?PHP
-	//include ("phpPublicFiles/initIndex.php");
-	
-		if (!isset($_GET["p"])) include ("indexStart.php");
-		else
-		{
-			$p = str_replace(".", "/", $_GET["p"]);
-			if (file_exists($p.".php")) include ($p.".php");
-			else include ("indexStart.php");
-		}
+<?php
+     $subdomain = 'cancun';
+     require_once $_SERVER['DOCUMENT_ROOT']. '/' . $subdomain . '/Twig/Autoloader.php';
+     Twig_Autoloader::register();
+     $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/' . $subdomain . '/public_html/');
+     $twig = new Twig_Environment($loader);
+     $url = "http://$_SERVER[HTTP_HOST]/".$subdomain."/?page=";
+     $urlCurrent = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+     $parameters = Array();
+
+     $parameters['domain'] = $url;
+
+     switch ("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]") {
+          // HOME
+          case $url."master":
+               // $parameters['page'] = 'home';
+               echo $twig->render('views/master/home.html', $parameters);
+               break;
+          case $url."sr-hotels":
+               // $parameters['page'] = 'home';
+               echo $twig->render('views/hotels/sr-hotels.html', $parameters);
+               break;
+          case $url."sr-hotels-flights":
+               // $parameters['page'] = 'home';
+               echo $twig->render('views/results/sr-hotels-flights.html', $parameters);
+               break;
+          case $url."hotel-landing":
+               // $parameters['page'] = 'home';
+               echo $twig->render('views/hotels/landing.html', $parameters);
+               break;
+          default:
+               $parameters['page'] = 'sitemap';
+               echo $twig->render('views/includes/sitemap.html', $parameters);
+               break;
+     }
 ?>
