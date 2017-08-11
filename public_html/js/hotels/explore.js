b813169f95d8fd53f3406081d23241b5c8106c43
mlane <mlane@partnerfusion.com>
2017-08-10 19:01:59 -0600

    Explore V4

--- public_html/js/hotels/explore.js
+++ public_html/js/hotels/explore.js
@@ -30,71 +30,7 @@ var page = {
                galleryPopularStays.params.control = galleryPopularStays;
           }, true);
      },
-     binds: function() {
-          if (document.querySelector('a.mobile-filters-button')) {
-               for (var i = 0; i < document.querySelectorAll('a.mobile-filters-button').length; i++) {
-                    document.querySelectorAll('a.mobile-filters-button')[i].addEventListener('click', function() {
-                         f.addClass(document.querySelector('.mobile-search-bar'), 'open-filters');
-                         document.querySelector('a.mobile-filters-button').style.display = 'none';
-                    });
-               }
-          }
-
-          if (document.querySelector('a.close')) {
-               for (var i = 0; i < document.querySelectorAll('a.close').length; i++) {
-                    document.querySelectorAll('a.close')[i].addEventListener('click', function() {
-                         f.removeClass(document.querySelector('form.mobile-search-bar.open-filters'), 'open-filters');
-                         document.querySelector('a.mobile-filters-button').style.display = 'block';
-                    });
-               }
-          }
-
-          if (document.querySelector('a.more-filters')) {
-               for (var i = 0; i < document.querySelectorAll('a.more-filters').length; i++) {
-                    document.querySelectorAll('a.more-filters')[i].addEventListener('click', function() {
-                         document.querySelector('.additional-filters').style.display = 'block';
-                         document.querySelector('a.more-filters').style.display = 'none';
-                         document.querySelector('.mobile-search-bar button').style.display = 'none';
-                    });
-               }
-          }
-
-          if (document.querySelector('a.hide-filters')) {
-               for (var i = 0; i < document.querySelectorAll('a.hide-filters').length; i++) {
-                    document.querySelectorAll('a.hide-filters')[i].addEventListener('click', function() {
-                         document.querySelector('.additional-filters').style.display = 'none';
-                         document.querySelector('a.more-filters').style.display = 'block';
-                         document.querySelector('.mobile-search-bar button').style.display = 'block';
-                    });
-               }
-          }
-          if (document.querySelector('a.desktop-filters')) {
-               for (var i = 0; i < document.querySelectorAll('a.desktop-filters').length; i++) {
-                    document.querySelectorAll('a.desktop-filters')[i].addEventListener('click', function() {
-                         var selector = this.parentNode.querySelectorAll('.filter-box');
-                         var button = this.style.display = 'none';
-                         f.toggleClass(this.querySelector('i'), 'active');
-
-                         for (var i = 0; i < selector.length; i++) {
-                              f.toggleClass(selector[i], 'active');
-                         }
-                    });
-               }
-          }
-          if (document.querySelector('a.more-filters-link')) {
-               for (var i = 0; i < document.querySelectorAll('a.more-filters-link').length; i++) {
-                    document.querySelectorAll('a.more-filters-link')[i].addEventListener('click', function() {
-                         var selector = this.parentNode.parentNode.parentNode.querySelectorAll('.more-filters');
-                         f.toggleClass(this.querySelector('i'), 'active');
-
-                         for (var i = 0; i < selector.length; i++) {
-                              f.toggleClass(selector[i], 'active');
-                         }
-                    });
-               }
-          }
-
-     }
+     binds: function() {}
 }
 
 page.init();
